ae1ee35dc0737a3498f54ab8b34c6f7dd3daf239
Miłosz Czajkowski <noisequintet@wp.pl>
2016-01-10 20:33:27 +0100

    Number One

---
+++ KCK_BOT/KCK_BOT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCK_BOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
