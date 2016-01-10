da2d1742936d5eff45293260cefbe038eacef4a8
Miłosz Czajkowski <noisequintet@wp.pl>
2016-01-10 20:41:20 +0100

    AI BOTA

---
+++ BOTKCKOWY/BOTKCKOWY/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace KCK_BOT
+{
+    class Program
+    {
+       static string Input;
+        static void Main(string[] args)
+        {
+            Bot Bot = new Bot("Czarek");
+            Console.ReadLine();
+        }
+    }
+    }
